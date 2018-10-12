ef54f0c6d621166500d714a3b3ceac247a09119f
katyamatya21 <katyamatya21@gmail.com>
2018-10-12 22:04:53 +0300

    Добавляет фильтры для видео

--- docs/js/video.js
+++ docs/js/video.js
@@ -17,6 +17,11 @@ buttonMenu.addEventListener('click', function () {
   function Video(item) {
     this.item = item;
     this.video = null;
+    this.canvas = null;
+    this.context = null;
+
+    this.brightness = 0;
+    this.contrast = 0;
 
     this.init();
   }
@@ -28,12 +33,17 @@ buttonMenu.addEventListener('click', function () {
     this.video = this.createVideo();
     this.initVideo(this.video, this.item.dataset.source);
 
-    var canvas = this.item.querySelector('canvas');
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientHeight;
-    this.drawVideo(this.video, canvas);
+    this.canvas = this.item.querySelector('canvas');
+    this.context = this.canvas.getContext('2d');
+
+    this.canvas.width = this.canvas.clientWidth;
+    this.canvas.height = this.canvas.clientHeight;
 
     this.item.querySelector('.video__player').addEventListener('click', this.show.bind(this));
+    this.item.querySelector('.video__control--brightness input').addEventListener('change', this.changeBrightness.bind(this));
+    this.item.querySelector('.video__control--contrast input').addEventListener('change', this.changeContrast.bind(this));
+
+    this.video.addEventListener('play', this.onPlay.bind(this));
   };
 
   /**
@@ -72,37 +82,99 @@ buttonMenu.addEventListener('click', function () {
   };
 
   /**
-   * Draw on canvas
-   * @param video
-   * @param canvas
+   * Draw frame
    */
-  Video.prototype.drawVideo = function (video, canvas) {
-    var ctx = canvas.getContext('2d');
+  Video.prototype.onPlay = function () {
 
-    video.addEventListener('play', function () {
-      var $this = this;
+    var self = this;
 
-      (function loop() {
-        if (!$this.paused && !$this.ended) {
+    (function loop() {
+      if (!self.video.paused && !self.video.ended) {
 
-          canvas.width = canvas.clientWidth;
-          canvas.height = canvas.clientHeight;
+        self.canvas.width = self.canvas.clientWidth;
+        self.canvas.height = self.canvas.clientHeight;
 
-          var scale = Math.min(
-            canvas.width / $this.videoWidth,
-            canvas.height / $this.videoHeight) * 1.2;
+        var scale = Math.min(
+          self.canvas.width / self.video.videoWidth,
+          self.canvas.height / self.video.videoHeight) * 1.2;
 
-          var vidH = $this.videoHeight;
-          var vidW = $this.videoWidth;
-          var top = canvas.height / 2 - (vidH / 2) * scale;
-          var left = canvas.width / 2 - (vidW / 2) * scale;
+        var vidH = self.video.videoHeight;
+        var vidW = self.video.videoWidth;
+        var top = self.canvas.height / 2 - (vidH / 2) * scale;
+        var left = self.canvas.width / 2 - (vidW / 2) * scale;
 
-          ctx.drawImage($this, left, top, vidW * scale, vidH * scale);
-          setTimeout(loop, 1000 / 60);
-        }
-      })();
+        self.context.drawImage(self.video, left, top, vidW * scale, vidH * scale);
 
-    });
+        var imageData = self.context.getImageData(0, 0, self.canvas.width, self.canvas.height);
+        var data = imageData.data;
+
+        self.applyBrightness(data, self.brightness);
+        self.applyContrast(data, parseInt(self.contrast, 10));
+
+        self.context.putImageData(imageData, 0, 0);
+
+        setTimeout(loop, 1000 / 60);
+      }
+    })();
+  };
+
+  /**
+   * Make sure the value stay between 0 and 255
+   * @param value
+   * @returns {*}
+   */
+  Video.prototype.truncateColor = function (value) {
+    if (value < 0) {
+      value = 0;
+    } else if (value > 255) {
+      value = 255;
+    }
+
+    return value;
+  };
+
+  /**
+   * Set brightness
+   * @param data
+   * @param brightness
+   */
+  Video.prototype.applyBrightness = function (data, brightness) {
+    for (var i = 0; i < data.length; i += 4) {
+      data[i] += 255 * (brightness / 100);
+      data[i + 1] += 255 * (brightness / 100);
+      data[i + 2] += 255 * (brightness / 100);
+    }
+  };
+
+  /**
+   * Set contrast
+   * @param data
+   * @param contrast
+   */
+  Video.prototype.applyContrast = function (data, contrast) {
+    var factor = (259 * (contrast + 255)) / (255 * (259 - contrast));
+
+    for (var i = 0; i < data.length; i += 4) {
+      data[i] = this.truncateColor(factor * (data[i] - 128) + 128);
+      data[i + 1] = this.truncateColor(factor * (data[i + 1] - 128) + 128);
+      data[i + 2] = this.truncateColor(factor * (data[i + 2] - 128) + 128);
+    }
+  };
+
+  /**
+   * Change brightness
+   * @param event
+   */
+  Video.prototype.changeBrightness = function (event) {
+    this.brightness = event.target.value;
+  };
+
+  /**
+   * Change contrast
+   * @param event
+   */
+  Video.prototype.changeContrast = function (event) {
+    this.contrast = event.target.value;
   };
 
   /**
@@ -125,10 +197,9 @@ buttonMenu.addEventListener('click', function () {
   Video.prototype.hide = function (event) {
     var video = event.target.parentNode.parentNode;
     video.classList.remove('video--show');
+    video.style = 'z-index: 666;';
 
     this.video.muted = true;
-
-    video.style = 'z-index: 666;';
 
     setTimeout(function () {
       video.style = '';
